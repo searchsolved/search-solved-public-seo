5f1eb6790a3cfaaa87ce0f96cd7ea6c3339c946a
lee <data.services@searchsolved.co.uk>
2021-01-11 16:11:07 +0000

    Small readability code tweaks.

--- Internal Search Mapper/internal-search-mapper.py
+++ Internal Search Mapper/internal-search-mapper.py
@@ -4,18 +4,15 @@ internal searches to.
 
 1) Search Terms Report must be exported as an Excel file
 2) Screaming Frog Crawl should only contain category pages, (unless you want to map internal searches to products!)
-3) Paths are hardcoded, please enter your own for GA, Screaming Frog and the Output.CSV
-"""
+3) Paths are hardcoded, please enter your own for GA, Screaming Frog and the Output.CSV"""
 
+from glob import glob
+import pandas as pd
 
 # set the folder paths HERE for your input files!
 ga_path = "C:\path-to-folder-with-ga-export"  # enter the path the the folder that contains your GA search terms export
 sf_path = "C:\path-to-folder-with-sf-export"  # enter the path of the folder that contains internal_html.csv file
 export_path = "C:\path-to-export-folder"  # enter the path to export the output.csv file to
-
-
-from glob import glob  # Used to parse wildcards for GA export file name as it is unique everytime. No need to rename!
-import pandas as pd
 
 # adds file names to the paths.
 ga_path_file = ga_path + "/Analytics*.xlsx"
@@ -26,43 +23,38 @@ export_path_file = export_path + "/output.csv"
 for f in glob(ga_path_file):
     df_ga = pd.read_excel((f), sheet_name="Dataset1")
 
+# imports screaming frog columns
 df_sf = pd.read_csv(sf_path_file, encoding="utf8")[["H1-1", "Address", "Indexability"]]
 
-
-df_sf["H1-1"] = df_sf["H1-1"].str.lower()  # convert to lower case for matching
+# convert to lower case for matching
+df_sf["H1-1"] = df_sf["H1-1"].str.lower()
 df_ga["Search Term"] = df_ga[
     "Search Term"
 ].str.lower()  # convert to lower case for matching
 df_ga.drop_duplicates(subset=["Search Term"], inplace=True)  # drop duplicates
 
-try:  # drop non-indexable pages
-    df_sf = df_sf[
-        ~df_sf["Indexability"].isin(["Non-Indexable"])
-    ]  # Drop Non-indexable Pages
+# drop non-indexable pages
+try:
+    df_sf = df_sf[~df_sf["Indexability"].isin(["Non-Indexable"])]
 except Exception:
     pass
 
-del df_sf["Indexability"]  # delete the helper column
+# delete the helper column
+del df_sf["Indexability"]  
 
+# merge the dataframe
+final_df = pd.merge(df_sf, df_ga, left_on="H1-1", right_on="Search Term", how="inner")
 
-final_df = pd.merge(
-    df_sf, df_ga, left_on="H1-1", right_on="Search Term", how="inner"
-)  # merge the dataframe
+# sort by opportunity
+final_df = final_df.sort_values(by="Total Unique Searches", ascending=False)
 
+# Round Float to two decimal places
+final_df = final_df.round(decimals=2)  
 
-final_df = final_df.sort_values(
-    by="Total Unique Searches", ascending=False
-)  # sort by opportunity
+# renames the cols
+final_df.rename(columns={"H1-1": "Matched H1", "Address": "Matched URL"}, inplace=True)
 
-
-final_df = final_df.round(decimals=2)  # Round Float to two decimal places
-
-
-final_df.rename(
-    columns={"H1-1": "Matched H1", "Address": "Matched URL"}, inplace=True
-)  # renames the cols
-
-
+# set new column order for final df
 cols = [
     "Search Term",
     "Matched H1",
@@ -75,7 +67,8 @@ cols = [
     "Avg. Search Depth",
 ]
 
-final_df = final_df.reindex(columns=cols)  # re-index columns into a logical order
+# re-index columns into a logical order
+final_df = final_df.reindex(columns=cols)
 
-
-final_df.to_csv(export_path_file, index=False)  # export the final csv
+# export the final csv
+final_df.to_csv(export_path_file, index=False)

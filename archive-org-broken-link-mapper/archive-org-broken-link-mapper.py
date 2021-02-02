e74f74e7f1d36cf454bfc07a65e811b30849dcee
lee <data.services@searchsolved.co.uk>
2021-02-02 21:25:38 +0000

    v1 release
    rough around the edges!

---
+++ archive-org-broken-link-mapper/archive-org-broken-link-mapper.py
@@ -0,0 +1,162 @@
+import requests as req
+import pandas as pd
+from io import StringIO
+from urllib.parse import urlparse
+from polyfuzz import PolyFuzz
+
+
+# read in the crawl file
+df_sf = pd.read_csv(
+    "/python_scripts/archive-org-broken-link-mapping/internal_html.csv"
+)[["Address", "Status Code", "Indexability"]]
+
+# extract the domain name from the crawl
+extracted_domain = df_sf["Address"]
+extracted_domain = extracted_domain.iloc[0]
+url = extracted_domain
+o = urlparse(url)
+domain = o.netloc
+
+print("Imported Crawl File ..")
+print("Detected Domain = ", domain)
+
+# append extracted domain to archive.org txt url
+archive_url = "http://web.archive.org/cdx/search/cdx?url=" + domain + "*&output=txt"
+
+# get the response
+print("Downloading URLs from Archive.org ..")
+resp = req.get(archive_url)
+
+# make the df and set the column names
+df_archive = pd.read_csv(
+    StringIO(resp.text),
+    sep=" ",
+    names=["1", "2", "Address", "Content Type", "5", "6", "7"],
+    dtype=str,
+)
+
+count_row = df_archive.shape[0]
+print("Downloaded", count_row, "URLs from Archive.org ..")
+
+# delete the unused columns
+del df_archive["1"]
+del df_archive["2"]
+del df_archive["5"]
+del df_archive["6"]
+del df_archive["7"]
+
+# replace port 80 urls before de-duping
+df_archive["Address"] = df_archive["Address"].str.replace("\:80", "")
+
+# drop all parameter urls (OPTIONAL!)
+df_archive["Address"] = df_archive["Address"].str.replace("\?", "")
+
+# drop duplicate urls
+df_archive.drop_duplicates(subset="Address", inplace=True)
+
+# keep only text/http content
+df_archive = df_archive[df_archive["Content Type"].isin(["text/html"])]
+df_archive["Address"].str.lower()
+
+# delete the helper column
+del df_archive["Content Type"]
+
+# drop additional non-html pages incorrectly flagged as text/html
+df_archive = df_archive[
+    ~df_archive["Address"].str.contains(
+        ".css|.js|.jpg|.png|.jpeg|.pdf|.JPG|.ico|ver=|.gif|.txt"
+    )
+]
+
+# drop marketing tags
+df_archive = df_archive[~df_archive["Address"].str.contains("utm|gclid")]
+
+# drop any additional rows (custom) (Drops brand pages and pagination)
+df_archive = df_archive[~df_archive["Address"].str.contains("/brand|/page")]
+
+# drop basket related URLs [Should be blocked in robots.txt anyway!]
+df_archive = df_archive[
+    ~df_archive["Address"].str.contains("basket|checkout|add|account")
+]
+
+# make the final df
+df_final = pd.merge(df_archive, df_sf, on="Address", how="left")
+
+print("Dropping URLs Found in the Crawl File ..")
+
+# drop archive.org rows which matched the crawl file
+df_final = df_final[~df_final["Indexability"].isin(["Indexable"])]
+df_final = df_final[~df_final["Indexability"].isin(["Non-Indexable"])]
+
+# delete the helper column
+del df_final["Indexability"]
+
+# count the rows
+count_row = df_final.shape[0]
+
+print("Getting Live HTTP Status Codes for", count_row, "URLs Using Requests ..")
+
+# get remaining status codes using requests library
+def url_access(x):
+    return req.head(x).status_code
+
+
+df_final["Status Code"] = df_final["Address"].apply(url_access)
+
+# drop urls if already redirected
+df_final = df_final[~df_final["Status Code"].isin(["301"])]
+df_final = df_final[~df_final["Status Code"].isin(["302"])]
+
+print("Automatically Mapping URLs ..")
+
+# create lists from dfs
+df_final_list = list(df_final["Address"])
+df_sf_list = list(df_sf["Address"])
+
+# instantiate PolyFuzz model, choose TF-IDF as the similarity measure and match the two lists.
+model = PolyFuzz("TF-IDF").match(df_final_list, df_sf_list)
+
+# make the polyfuzz dataframe
+df_matches = model.get_matches()
+
+count_row = df_final.shape[0]
+print("Total Opportunity:", count_row, "URLs")
+
+df_stats = pd.merge(
+    df_matches, df_final, left_on="From", right_on="Address", how="inner"
+)
+
+# Sort on Similarity
+df_stats = df_stats.sort_values(by="Similarity", ascending=False)
+
+df_stats["Status Code"] = df_stats["Status Code"].astype(str)
+
+# replace status codes with plain English
+df_stats["Status Code"] = df_stats["Status Code"].str.replace("200", "200 - Orphaned")
+df_stats["Status Code"] = df_stats["Status Code"].str.replace(
+    "404", "404 - Not Redirected"
+)
+
+# delete address column appended from the merge
+del df_stats["Address"]
+
+# Defines the New Columns
+cols = ["From", "Status Code", "To", "Similarity"]
+
+# Re-indexes Columns To Place Them In A Logical Order + Inserts New Blank Columns for KW Checks.
+df_stats = df_stats.reindex(columns=cols)
+
+# rename the cols
+df_stats.rename(columns={"From": "Archive URL", "To": "Suggested URL"}, inplace=True)
+
+orphaned = df_stats["Status Code"].str.contains("200 - Orphaned").value_counts()[True]
+redirected = (
+    df_stats["Status Code"].str.contains("404 - Not Redirected").value_counts()[True]
+)
+
+print(orphaned, "Orphaned URLs ..")
+print(redirected, "404 URLs to Redirect ..")
+
+df_stats.to_csv("/archive-org-broken-link-mapping/output.csv")
+
+print("Finished!")

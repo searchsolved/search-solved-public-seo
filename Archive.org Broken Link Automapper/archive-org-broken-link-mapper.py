4c5eedd98778d91bf047c4279aa187675d057b6d
lee <data.services@searchsolved.co.uk>
2021-09-19 08:53:19 +0100

    Update archive-org-broken-link-mapper.py

--- Archive.org Broken Link Automapper/archive-org-broken-link-mapper.py
+++ Archive.org Broken Link Automapper/archive-org-broken-link-mapper.py
@@ -16,7 +16,8 @@ from requests.packages.urllib3.util.retry import Retry
 startTime = time.time()
 
 # set the user agent here
-user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+#user_agent = "Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)"
+user_agent = "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36"
 CONNECTIONS = 8  # increase this number to run more workers at the same time.
 
 # import the Screaming Frog crawl File (internal_html.csv)
@@ -71,6 +72,9 @@ print("Filtered to", remaining_count, "qualifying URLs!")
 # fetch the latest version of the archive.org url (so the h1's can be extracted with requests)
 url_list = list(df_archive['Address'])  # create list from address column
 
+df_archive.to_csv('/python_scripts/df_archive-line-75.csv')  # todo remove this
+
+
 archive_url_list = []
 
 def get_archive_url(url):
@@ -103,13 +107,16 @@ df_archive_urls["Extracted Archive URL"] = df_archive_urls["Extracted Archive UR
 df_archive_urls["Extracted Archive URL"] = df_archive_urls["Extracted Archive URL"].str.replace(',', '/')
 df_archive_urls.drop_duplicates(subset=['Extracted Archive URL'], keep="first", inplace=True)  # drop duplicates
 
+df_archive_urls.to_csv('/python_scripts/df_archive_urls.csv')  # todo delete this
+
+# todo this code needs fixing - threading appends to the list in the order the task completes rather than chronilogically
 
 # extract h1s from archive url df and make into a list
-archive_url_list = df_archive_urls['Extracted Archive URL']
+archive_url_list = list(df_archive_urls[0])
 archive_h1_list = []
 def get_archive_h1(h1_url):
     html = urlopen(h1_url)
-    bsh = BeautifulSoup(html.read(), 'html.parser')
+    bsh = BeautifulSoup(html.read(), 'lxml')
     return bsh.h1.text.strip()
 
 def concurrent_calls():
@@ -128,9 +135,9 @@ if __name__ == '__main__':
     print(archive_h1_list)
 
 # clean up the dataframe
-df_archive.to_csv('/python_scripts/df_archive-130.csv')  # todo remove
 
 df_archive_urls['H1'] = archive_h1_list  # add list to dataframe column
+df_archive.to_csv('/python_scripts/df_archive-130.csv')  # todo remove
 df_archive_urls = df_archive_urls[~df_archive_urls["H1"].isin(["Got an HTTP 301 response at crawl time"])]  # drop
 df_archive = pd.merge(df_archive, df_archive_urls, left_on="Address", right_on="Extracted Archive URL", how="inner")
 df_archive.to_csv('/python_scripts/df_archive-133.csv')  # todo remove
@@ -139,7 +146,7 @@ df_archive.to_csv('/python_scripts/df_archive-133.csv')  # todo remove
 df_archive = df_archive[df_archive["H1"].notna()]
 df_archive = df_archive[~df_archive["H1"].str.contains("No Data Received!", na=False)]
 
-df_archive.to_csv('/python_scripts/df_archive-138.csv')  # todo remove
+df_archive.to_csv('/python_scripts/df_archive-138.csv')  # todo remove [ archive h1s wrong here]
 
 df_sf = df_sf[df_sf["H1-1"].notna()]
 df_sf_list = list(df_sf["H1-1"])
@@ -148,7 +155,7 @@ df_archive_list = list(df_archive["H1"])
 # instantiate PolyFuzz model, choose TF-IDF as the similarity measure and match the two lists.
 model = PolyFuzz("TF-IDF").match(df_archive_list, df_sf_list)
 df_matches = model.get_matches()  # make the polyfuzz dataframe
-df_matches.to_csv('/python_scripts/df_matches.csv')  #todo remove
+df_matches.to_csv('/python_scripts/df_matches-156.csv')  #todo remove
 
 # make mini dfs for easier matching
 df_archive_mini = df_archive[["H1", "Extracted Archive URL"]]
@@ -157,13 +164,14 @@ df_sf_mini = df_sf[["H1-1", "Address"]]
 df_matches = pd.merge(df_matches, df_archive_mini, left_on="From", right_on="H1")
 df_matches = pd.merge(df_matches, df_sf_mini, left_on="To", right_on="H1-1")
 
-df_matches.to_csv('/python_scripts/df_matches.csv')  #todo remove
-df_matches.drop_duplicates(subset=['Extracted Archive URL'], keep="first", inplace=True)
+df_matches.to_csv('/python_scripts/df_matches.csv')  #todo remove - h1s are good here
+#df_matches.drop_duplicates(subset=['Extracted Archive URL'], keep="first", inplace=True)
 df_matches.rename(columns={"H1": "Archive H1", "Extracted Archive URL": "Archive URL", "H1-1": "Matched H1", "Address": "Matched URL"}, inplace=True)
 cols = "Archive URL", "Archive H1", "Similarity", "Matched URL", "Matched H1", "Final HTTP Status"
 df_matches = df_matches.reindex(columns=cols)
-
+df_matches.to_csv('/python_scripts/df_matches-170.csv') #todo remove
 # check http status of recovered archive.org url using requests
+
 headers = {'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/39.0.2171.95 Safari/537.36'}
 logging.basicConfig(level=logging.DEBUG)
 s = req.Session()

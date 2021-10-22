823506120aae50342bad771a6325b6c57087f314
searchsolved <72097373+searchsolved@users.noreply.github.com>
2021-10-22 18:36:23 +0100

    Update keyword_clustering_steamlit.py

--- Keyword_Clustering_Steamlit/keyword_clustering_steamlit.py
+++ Keyword_Clustering_Steamlit/keyword_clustering_steamlit.py
@@ -181,14 +181,3 @@ try:
 except NameError:
     pass
 
-def get_table_download_link(df):
-    """Generates a link allowing the data in a given panda dataframe to be downloaded
-    in:  dataframe
-    out: href string
-    """
-    csv = df_matched.to_csv(index=False)
-    b64 = base64.b64encode(csv.encode()).decode()  # some strings <-> bytes conversions necessary here
-    href = f'<a href="data:file/csv;base64,{b64}">Download csv file</a>'  
-
-st.markdown(get_table_download_link(df_matched), unsafe_allow_html=True)
-

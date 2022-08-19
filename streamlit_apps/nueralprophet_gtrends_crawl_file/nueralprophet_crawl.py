f6f7239bcec91efb22cd467f5448aa81cf34087f
Search Solved <72097373+searchsolved@users.noreply.github.com>
2022-08-19 15:29:40 +0100

    Update nueralprophet_crawl.py

--- streamlit_apps/nueralprophet_gtrends_crawl_file/nueralprophet_crawl.py
+++ streamlit_apps/nueralprophet_gtrends_crawl_file/nueralprophet_crawl.py
@@ -18,7 +18,8 @@ import time
 set_random_seed(0)
 
 st.write(
-    "Made in [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/) by [@LeeFootSEO](https://twitter.com/LeeFootSEO) / [![this is an image link](https://i.imgur.com/bjNRJra.png)](https://www.buymeacoffee.com/leefootseo) [Support My Work! Buy me a coffee!](https://www.buymeacoffee.com/leefootseo)")
+    "[![this is an image link](https://i.imgur.com/Ex8eeC2.png)](https://www.patreon.com/leefootseo) [Become a Patreon for Early Access, Support & More!](https://www.patreon.com/leefootseo)  |  Made in [![this is an image link](https://i.imgur.com/iIOA6kU.png)](https://www.streamlit.io/) by [@LeeFootSEO](https://twitter.com/LeeFootSEO)")
+
 st.title("Google Trends & Facebook Prophet Tool")
 
 # streamlit variables

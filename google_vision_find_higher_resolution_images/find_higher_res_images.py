40dde50e25cebf1078f1dd49f2d0296f37c2c09c
Search Solved <72097373+searchsolved@users.noreply.github.com>
2023-03-24 10:27:15 +0000

    More fixes

--- google_vision_find_higher_resolution_images/find_higher_res_images.py
+++ google_vision_find_higher_resolution_images/find_higher_res_images.py
@@ -1,23 +1,41 @@
-import logging
 import os
-from typing import List
-from tqdm import tqdm
-
-
 import pandas as pd
-import requests
-from google.cloud import vision
 from PIL import Image
+import requests
+import concurrent.futures
+import logging
+from typing import List
+from urllib.parse import urlparse
+from google.cloud import vision
+from tqdm import tqdm
+import urllib3.exceptions
+import google.api_core.exceptions
 
 header = {
-    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/87.0.4280.88 Mobile Safari/537.36'
+    'User-Agent': 'Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) '
+                  'AppleWebKit/537.36 (KHTML, like Gecko) '
+                  'Chrome/87.0.4280.88 Mobile Safari/537.36'
 }
-
 
 # read in the json secrets file and instantiate google vision
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "/python_scripts/cloud_vision_api.json"
 client = vision.ImageAnnotatorClient()
 
+# set up logging
+logging.basicConfig(filename='image_scraper.log', level=logging.ERROR)
+
+# define a function to retrieve image size from metadata
+def get_image_size(url: str) -> tuple:
+    try:
+        r = requests.head(url, headers=header, timeout=30)
+        if 'content-length' in r.headers:
+            return int(r.headers['content-length'])
+        else:
+            with requests.get(url, headers=header, timeout=30, stream=True) as r:
+                return int(r.headers['content-length'])
+    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError, requests.exceptions.Timeout, urllib3.exceptions.MaxRetryError) as e:
+        logging.error(f'Error retrieving image size for {url}: {e}')
+        return 0
 
 # define a function to retrieve image URLs from Google Vision API
 def get_image_urls(url: str) -> List[str]:
@@ -26,75 +44,67 @@ def get_image_urls(url: str) -> List[str]:
         response = client.web_detection(image=image).web_detection
         urls = [img.url for img in response.full_matching_images]
         return urls
-    except Exception as e:
+    except (google.api_core.exceptions.GoogleAPIError, urllib3.exceptions.MaxRetryError) as e:
         logging.error(f'Error retrieving image URLs for {url}: {e}')
         return []
-
-
-# define a function to get the size of an image from its URL
-def get_image_size(url: str) -> tuple:
-    try:
-        im = Image.open(requests.get(url, headers=header, timeout=20, stream=True, verify=False).raw)
-        return im.size
-    except Exception as e:
-        logging.error(f'Error opening {url}: {e}')
-        return (0, 0)
-
 
 # define a function to process a single URL
 def process_url(url: str) -> pd.DataFrame:
     result = {'original_url': url, 'matching_imgs': [], 'width_diff': [], 'height_diff': []}
     try:
-        og_image = Image.open(requests.get(url, headers=header, timeout=20, stream=True, verify=True).raw)
-        og_width, og_height = og_image.size
-
-        img_urls = []
-        image = vision.Image(source=vision.ImageSource(image_uri=url))
-        response = client.web_detection(image=image).web_detection
-        for img in tqdm(response.full_matching_images, desc=f"Processing {url}", unit="image"):
-            try:
-                matching_image = Image.open(requests.get(img.url, headers=header, timeout=20, stream=True, verify=True).raw)
-            except requests.exceptions.SSLError as e:
-                logging.warning(f"SSL error processing {img.url}: {e}")
-                continue
-            matching_width, matching_height = matching_image.size
-            img_urls.append(img.url)
-            result['matching_imgs'].append(img.url)
-            result['width_diff'].append(matching_width - og_width)
-            result['height_diff'].append(matching_height - og_height)
-
-        result['width_matching_imgs'] = [Image.open(requests.get(url, headers=header, timeout=20, stream=True, verify=True).raw).size[0] for url in img_urls]
-        result['height_matching_imgs'] = [Image.open(requests.get(url, headers=header, timeout=20, stream=True, verify=True).raw).size[1] for url in img_urls]
-    except Exception as e:
+        og_size = get_image_size(url)
+        og_width, og_height = Image.open(requests.get(url, headers=header, timeout=20, stream=True).raw).size
+        img_urls = get_image_urls(url)
+        for img_url in img_urls:
+            img_size = get_image_size(img_url)
+            if img_size > og_size:
+                img_width, img_height = Image.open(requests.get(img_url, headers=header, timeout=20, stream=True).raw).size
+                result['matching_imgs'].append(img_url)
+                result['width_diff'].append(img_width - og_width)
+                result['height_diff'].append(img_height - og_height)
+    except (requests.exceptions.SSLError, requests.exceptions.ConnectionError, requests.exceptions.Timeout, urllib3.exceptions.MaxRetryError, google.api_core.exceptions.GoogleAPIError, OSError, ValueError) as e:
         logging.error(f'Error processing {url}: {e}')
-        return pd.DataFrame(result)
-
     return pd.DataFrame(result)
 
-
 # read in the datafile of image urls
-df = pd.read_csv('/python_scripts/google_vision/input_file/wc_images_small.csv')
+df = pd.read_csv('/python_scripts/google_vision/input_file/wc_images.csv')
 
-# process URLs in parallel using all available CPU cores
-df_results = pd.concat([process_url(url) for url in tqdm(df['Images'], desc="Processing URLs")], ignore_index=True)
+# process the URLs in parallel using a thread pool
+results = []
+with concurrent.futures.ThreadPoolExecutor() as executor:
+    futures = [executor.submit(process_url, url) for url in df['Images']]
+    for future in tqdm(concurrent.futures.as_completed(futures), total=len(futures)):
+        try:
+            results.append(future.result())
+        except Exception as e:
+            logging.error(f'Error processing future: {e}')
 
-# merge the data with the original DataFrame
-df = pd.merge(df, df_results, on='original_url', how='outer')
+# concatenate the results into a single DataFrame
+df = pd.concat(results)
+df = df.assign(height_matching_imgs=df['matching_imgs'].apply(lambda url: Image.open(requests.get(url, headers=header, timeout=20, stream=True).raw).size[1]))
+df = df.assign(width_matching_imgs=df['matching_imgs'].apply(lambda url: Image.open(requests.get(url, headers=header, timeout=20, stream=True).raw).size[0]))
+
+# calculate source image size
+df[['width_source_img', 'height_source_img']] = df['original_url'].apply(lambda url: Image.open(requests.get(url, headers=header, timeout=20, stream=True).raw).size).apply(pd.Series)
+
+# calculate size difference
+df['width_diff'] = df['width_matching_imgs'] - df['width_source_img']
+df['height_diff'] = df['height_matching_imgs'] - df['height_source_img']
 
 # drop image recommendations that are the same width/height or smaller
 df = df.loc[(df['width_diff'] > 0) & (df['height_diff'] > 0)]
 
-# drop duplicates if both width and height and duplicated
-df.drop_duplicates(subset=["original_url", "width_diff", "height_diff"], keep="first", inplace=True)
+# drop duplicates if both width and height are duplicated
+df.drop_duplicates(subset=["width_diff", "height_diff"], keep="first", inplace=True)
 
 # sort on highest values
 df.sort_values(["width_diff", "height_diff"], ascending=[False, False], inplace=True)
 
-# keep the top 3 highest values
-df = df.groupby(['original_url']).head(3)
+# keep the top 3 highest values for each source image
+df = df.groupby(['original_url']).head(10)
 
-# reorder the columns
-df = df[['original_url', 'matching_imgs', 'width_diff', 'height_diff', 'width_matching_imgs', 'height_matching_imgs']]
+# re-order the columns
+df = df[['original_url', 'matching_imgs', 'width_diff', 'height_diff', 'width_matching_imgs', 'height_matching_imgs', 'width_source_img', 'height_source_img']]
 
-# save the results to a CSV file
+# write results to CSV
 df.to_csv("/python_scripts/higher_resolution_images_all.csv", index=False)

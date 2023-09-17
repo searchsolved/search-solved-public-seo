109c786faa42874b23a33125fad40878956ea1e9
Search Solved <72097373+searchsolved@users.noreply.github.com>
2023-09-17 18:50:54 +0100

    Update crawl2category.py
    Fixes and updates. Release candidate 1

--- portfolio/crawl2category/streamlit-app/crawl2category.py
+++ portfolio/crawl2category/streamlit-app/crawl2category.py
@@ -1,6 +1,5 @@
 import streamlit as st
 import string
-import os
 import nltk
 import numpy as np
 import pandas as pd
@@ -10,12 +9,12 @@ import inflect
 import chardet
 from nltk.corpus import stopwords
 from collections import Counter
+import traceback
 
 # Initialize the inflect engine
 p = inflect.engine()
 nltk.download('punkt')
 st.set_page_config(page_title="Product2Category - Automatic Category Suggestion Tool", layout="wide")
-
 
 # Check if stopwords are already downloaded
 try:
@@ -29,18 +28,27 @@ stop_words = set(stopwords.words('english'))
 # Load the pre-trained Sentence Transformer model
 model = SentenceTransformer('paraphrase-MiniLM-L3-v2')  # Fastest
 
-
 def clean_text(text):
-    text = ''.join([char if char not in string.punctuation else ' ' for char in text])
-    text = ' '.join(text.split())
-    text = ' '.join([word for word in text.split() if len(word) > 1])
+    return ''.join([c if c not in string.punctuation else ' ' for c in text]).lower()
+
+def detect_file_encoding(file):
+    rawdata = file.read(10000)
+    file.seek(0)  # Ensure the file pointer is reset after reading
+    result = chardet.detect(rawdata)
+    return result['encoding']
+
+def filter_combinations(product_name, combinations, threshold):
+    if not combinations:
+        return []
+    similarities = semantic_similarity_batch([product_name], combinations)[0]
+    return [combo for idx, combo in enumerate(combinations) if similarities[idx] > threshold]
+
+def pluralize_except_numbers(text):
+    if isinstance(text, str):
+        if text[-1].isdigit():
+            return text
+        return p.plural(text)
     return text
-
-
-def remove_stopwords(text):
-    tokens = nltk.word_tokenize(text)
-    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
-    return ' '.join(filtered_tokens)
 
 
 def generate_ngrams(text, n):
@@ -52,6 +60,11 @@ def generate_ngrams(text, n):
     return ngrams
 
 
+def remove_stopwords(text):
+    tokens = nltk.word_tokenize(text)
+    filtered_tokens = [word for word in tokens if word.lower() not in stop_words]
+    return ' '.join(filtered_tokens)
+
 def semantic_similarity_batch(texts1, texts2):
     vectors1 = model.encode(texts1)
     vectors2 = model.encode(texts2)
@@ -61,42 +74,41 @@ def semantic_similarity_batch(texts1, texts2):
     return dot_products / (norms1[:, None] * norms2)
 
 
-def filter_combinations(product_name, combinations, threshold):
-    if not combinations:
-        return []
-    similarities = semantic_similarity_batch([product_name], combinations)[0]
-    return [combo for idx, combo in enumerate(combinations) if similarities[idx] > threshold]
-
-
-def pluralize_except_numbers(text):
-    if isinstance(text, str):
-        if text[-1].isdigit():
-            return text
-        return p.plural(text)
-    return text
-
-
-def process_data(uploaded_file, selected_column, n_grams, similarity_threshold, override_delimiter):
+def process_data(uploaded_file, file_type, selected_column, n_grams, similarity_threshold, override_delimiter):
     try:
-        uploaded_file.seek(0)
-        raw_bytes = uploaded_file.read()
-        result = chardet.detect(raw_bytes)
-        file_encoding = result['encoding'].lower()
-
-        if file_encoding == 'utf-8':
-            df = pd.read_csv(uploaded_file, on_bad_lines='skip', low_memory=False)
-        elif file_encoding == 'utf-16':
-            df = pd.read_csv(uploaded_file, on_bad_lines='skip', low_memory=False, encoding='utf-16')
+        if 'excel' in file_type or 'spreadsheetml' in file_type:
+            try:
+                df = pd.read_excel(uploaded_file, engine='openpyxl')
+            except Exception as e:
+                st.error(f"An error occurred while reading the Excel file: {str(e)}")
+                return None
         else:
-            st.error(f"Unsupported encoding: {file_encoding}")
-            return None
+            # Resetting to the beginning of the file
+            uploaded_file.seek(0)
+
+            # Detect file encoding
+            raw_bytes = uploaded_file.read()
+            result = chardet.detect(raw_bytes)
+            file_encoding = result['encoding'].lower()
+
+            # Resetting to the beginning of the file again before reading it as a dataframe
+            uploaded_file.seek(0)
+
+            if file_encoding == 'utf-8' or file_encoding == 'utf-8-sig':
+                df = pd.read_csv(uploaded_file, on_bad_lines='skip', low_memory=False)
+            elif file_encoding == 'utf-16':
+                df = pd.read_csv(uploaded_file, on_bad_lines='skip', low_memory=False, encoding='utf-16')
+            else:
+                st.error(f"Unsupported encoding: {file_encoding}")
+                return None
 
         df = df[df[selected_column].notna()]
 
         if override_delimiter:
             delimiter = override_delimiter
         else:
-            delimiter_counts = Counter("".join(df['Categories'].dropna().tolist()))
+            delimiter_counts = Counter("".join(df[selected_column].dropna().tolist()))
+
             for char in string.ascii_letters + string.digits + " ":
                 del delimiter_counts[char]
             delimiter = max(delimiter_counts, key=delimiter_counts.get)
@@ -104,7 +116,7 @@ def process_data(uploaded_file, selected_column, n_grams, similarity_threshold, 
         # Display the detected delimiter
         st.info(f"Detected delimiter: '{delimiter}'")
         existing_categories = set()
-        for categories in df['Categories'].dropna():
+        for categories in df[selected_column].dropna():
             existing_categories.update(categories.split(delimiter))
         existing_categories = {clean_text(cat) for cat in existing_categories}
 
@@ -113,8 +125,13 @@ def process_data(uploaded_file, selected_column, n_grams, similarity_threshold, 
         # Progress bar for Streamlit
         progress_bar = st.progress(0)
         processed_data = []
+
+        # Get the index of the selected column
+        selected_column_idx = df.columns.get_loc(
+            selected_column) + 1  # +1 because the first index in itertuples() is the row index
+
         for idx, row in enumerate(df.itertuples()):
-            processed_data.append(filter_combinations(getattr(row, selected_column), row.N_grams, similarity_threshold))
+            processed_data.append(filter_combinations(row[selected_column_idx], row.N_grams, similarity_threshold))
             progress_bar.progress((idx + 1) / len(df))
 
         df['Relevant Combinations'] = processed_data
@@ -190,19 +207,60 @@ if st.checkbox("Show Help Documentation"):
     """)
 
 # UI Elements for File Upload and Processing
-uploaded_file = st.file_uploader("Choose a file")
-if uploaded_file is not None:
-    df_preview = pd.read_csv(uploaded_file, nrows=5)
-    st.write('Preview of uploaded data:')
-    st.write(df_preview)
+uploaded_file = st.file_uploader("Choose a file", type=['csv', 'xlsx', 'xls'])
 
-    selected_column = st.selectbox("Select the column with product names", df_preview.columns.tolist())
+if uploaded_file is not None:
+    raw_bytes = uploaded_file.read()
+    st.write(raw_bytes[:1000])  # Print the first 1000 bytes to check its content
+    file_details = uploaded_file.type
+    st.write(f"Detected file type: {file_details}")
+
+    # Initialize df_preview
+    df_preview = None
+
+    # If the file is an excel file
+    if 'excel' in file_details or 'spreadsheetml' in file_details:
+        try:
+            df_preview = pd.read_excel(uploaded_file, nrows=5, engine='openpyxl')  # specify the engine
+        except Exception as e:
+            st.error(f"An error occurred while reading the Excel file: {str(e)}")
+            st.write(traceback.format_exc())
+
+    # Otherwise, if it's a CSV
+    elif 'csv' in file_details:
+        try:
+            # Determine the encoding of the uploaded file
+            uploaded_file.seek(0)
+            file_encoding = detect_file_encoding(uploaded_file)
+
+            # Now, proceed to read with the detected encoding
+            uploaded_file.seek(0)  # Resetting to the beginning of the file
+            df_preview = pd.read_csv(uploaded_file, nrows=5, encoding=file_encoding)
+        except pd.errors.EmptyDataError:
+            st.error("The uploaded CSV file does not contain any data or columns. Please upload a valid CSV file.")
+        except Exception as e:
+            st.error(f"An error occurred while reading the CSV file: {str(e)}")
+            st.write(traceback.format_exc())
+
+    else:
+        st.error("Unsupported file type. Please upload a CSV or Excel file.")
+
+    # If df_preview is successfully created
+    if df_preview is not None:
+        st.write('Preview of uploaded data:')
+        st.write(df_preview)
+
+        selected_column = st.selectbox("Select the column with product names", df_preview.columns.tolist())
+
     n_grams = st.slider("Select N for N-grams", 1, 10, 6)
     similarity_threshold = st.slider("Select similarity threshold", 0.1, 1.0, 0.6)
     override_delimiter = st.text_input("Override delimiter (leave empty to auto-detect)")
 
     if st.button('Process'):
-        result_df = process_data(uploaded_file, selected_column, n_grams, similarity_threshold, override_delimiter)
+        uploaded_file.seek(0)  # Reset the file pointer to the beginning
+        result_df = process_data(uploaded_file, file_details, selected_column, n_grams, similarity_threshold,
+                                 override_delimiter)
+
         st.write('Processed Data:')
         st.write(result_df)
 
